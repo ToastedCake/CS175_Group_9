8c2a5af304855f705c8433ebcf3e3fa9cf99ccf1
Dylan Chen <93622599+MeB4You@users.noreply.github.com>
2023-03-12 13:45:44 -0700

    a*search (has bug)

---
+++ a_find_entities.py
@@ -0,0 +1,384 @@
+from builtins import range
+import json
+import MalmoPython
+import os
+import sys
+import time
+import math
+import numpy as np
+
+class Vertex:
+    def __init__(self,x,y,z,row,col,parent,g,h):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.row = row # row, position in the graph
+        self.col = col # col, position in the graph
+        self.parent = parent
+        self.g = g
+        self.h = h
+        self.f = g+h
+    def __eq__(self,other):
+        return self.__dict__ == other.__dict__
+
+
+def create_Graph(agent_host):
+    # create graph from min (x,y,z) to max (x,y,z), following row major order
+    #
+    # Ex: start: min(-1,5,1), end: max(1,5,1). Before reshape observation is a 1d array [0,1,2,3,4,5,6,7,8] 
+    # After reshape to 3X3:
+    #                      North
+    #         (-1,5,-1)   (0,5,-1)           (1,5,-1)
+    # West    (-1,5,0)    (0,5,0)(Agent)     (1,5,0)      East
+    #         (-1,5,1)    (0,5,1)            (1,5,1)
+    #                      South
+    #
+    world_state = agent_host.getWorldState()
+    while world_state.number_of_observations_since_last_state == 0:
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        if world_state.is_mission_running == False:
+            return None
+    msg = world_state.observations[-1].text
+    observations = json.loads(msg)
+    graph = observations["grid_observation"] # 1d array
+    graph = np.reshape(graph,(199,199)) #center (99,99)  
+    center_x = 99 
+    center_y = 99 
+    return graph,center_x,center_y
+
+def heuristic(x,y,z,target_x,target_y,target_z):
+    # Manhattan Distance
+    return abs(x-target_x) +abs(y-target_y) + abs(z-target_z)
+
+def find_minf_vertex_and_remove(open_List):
+    # find vertex that has the minimum f(f=g+h)
+    # may have errors here due to unfamiliar with python
+    minf = sys.maxsize
+    minf_Vertex = None
+    # find the min f vertex
+    for vertex in open_List:
+        if(vertex.f < minf):
+            minf = vertex.f
+            minf_Vertex = vertex
+    # remove the min f vertex from the open_List
+    for i in range(0,len(open_List)):
+        if open_List[i] is minf_Vertex:
+            open_List.pop(i)
+            break
+
+    return minf_Vertex
+
+def isInBoudary(row,col):
+    if(row<0 or row >198):
+        return False
+    if(col<0 or col >198):
+        return False
+    return True
+
+def isInOpenList(v,open_List):
+    # if there already exist a node with the same(x,y,z) as the successor in open_List,
+    # and has a lower f (f=g+h) than successor, return true (we skip this successor)
+    # else return false (we need to furthur check the condition)
+
+    # may have errors here due to unfamiliar with python
+    for node in open_List:
+        if(node.x == v.x and node.y ==v.y and node.z == v.z ):
+            if(node.f <v.f):
+                return True
+            else:
+                return False
+    return False
+
+def isVisited(v,visited):
+    # if there already exist a node with the same(x,y,z) as the successor in visited,
+    # and has a lower f (f=g+h) in visited, return true (we skip this successor)
+    # else return false (we need to add the successor to open_List)
+
+    # may have errors here due to unfamiliar with python
+    for node in visited:
+        if(node.x == v.x and node.y ==v.y and node.z == v.z ):
+            if(node.f <v.f):
+                return True
+            else:
+                return False
+    return False
+
+def isDestination(v_x,v_y,v_z,dest_x,dest_y,dest_z):
+    if(v_x == dest_x and v_y == dest_y and v_z == dest_z):
+        return True
+    else:
+        return False
+    
+def isOnGraph(x,y,z,dest_x,dest_y,dest_z):
+    if(abs(x-dest_x)>99 or abs(y-dest_y)>0 or abs(z-dest_z)>99):
+        print("The destination is not on the graph")
+        return False
+    else:
+        return True
+
+def Astar_search(agent_host,agent_location, target_location):
+    graph,center_x,center_y = create_Graph(agent_host)
+    open_List = []
+    visited =[]
+    # start and destination coordinates 
+    s_x,s_y,s_z = agent_location[0],agent_location[1],agent_location[2]
+    d_x,d_y,d_z = target_location[0],target_location[1],target_location[2]
+
+    # check if the destination is on the graph
+    if(isOnGraph(s_x,s_y,s_z,d_x,d_y,d_z) == False):
+        return visited
+
+    h = heuristic(s_x,s_y,s_z,d_x,d_y,d_z)
+    start_vertex = Vertex(s_x,s_y,s_z,center_x,center_y,None,0,h)
+    
+    open_List.append(start_vertex)
+
+    while(len(open_List)>0):
+        q = find_minf_vertex_and_remove(open_List)
+        # create 4 successors
+        # north succssor
+        north_succssor_row = q.row - 1
+        north_succssor_col = q.col
+
+        # check if in boundary(graph) and its not an obstacle
+        if(isInBoudary(north_succssor_row,north_succssor_col)):
+            if(graph[north_succssor_row][north_succssor_col] == 'air'):
+                h = heuristic(q.x,q.y,(q.z-1),d_x,d_y,d_z)
+                north_succssor_vertex = Vertex(q.x,q.y,(q.z-1),north_succssor_row,north_succssor_col,q,(q.g+1),h)
+                if(isDestination(north_succssor_vertex.x,north_succssor_vertex.y,north_succssor_vertex.z,d_x,d_y,d_z) == True):
+                    # We find the destinaton, append q and this vertex to visited list and return
+                    visited.append(q)
+                    visited.append(north_succssor_vertex)
+                    return visited
+                else:
+                    if(isInOpenList(north_succssor_vertex,open_List) == False):
+                        if(isVisited(north_succssor_vertex,visited) == False):
+                            open_List.append(north_succssor_vertex)
+        # south successor
+        south_succssor_row = q.row + 1
+        south_succssor_col = q.col
+        
+        # check if in boundary(graph) and its not an obstacle
+        if(isInBoudary(south_succssor_row,south_succssor_col)):
+            if(graph[south_succssor_row][south_succssor_col] == 'air'):
+                h = heuristic(q.x,q.y,(q.z+1),d_x,d_y,d_z)
+                south_succssor_vertex = Vertex(q.x,q.y,(q.z+1),south_succssor_row,south_succssor_col,q,(q.g+1),h)
+                if(isDestination(south_succssor_vertex.x,south_succssor_vertex.y,south_succssor_vertex.z,d_x,d_y,d_z) == True):
+                    # We find the destinaton, append q and this vertex to visited list and return
+                    visited.append(q)
+                    visited.append(south_succssor_vertex)
+                    return visited
+                else:
+                    if(isInOpenList(south_succssor_vertex,open_List) == False):
+                        if(isVisited(south_succssor_vertex,visited) == False):
+                            open_List.append(south_succssor_vertex)
+
+        # east successor
+        east_succssor_row = q.row 
+        east_succssor_col = q.col + 1
+        
+        # check if in boundary(graph) and its not an obstacle
+        if(isInBoudary(east_succssor_row,east_succssor_col)):
+            if(graph[east_succssor_row][east_succssor_col] == 'air'):
+                h = heuristic((q.x+1),q.y,q.z,d_x,d_y,d_z)
+                east_succssor_vertex = Vertex((q.x+1),q.y,q.z,east_succssor_row,east_succssor_col,q,(q.g+1),h)
+                if(isDestination(east_succssor_vertex.x,east_succssor_vertex.y,east_succssor_vertex.z,d_x,d_y,d_z) == True):
+                    # We find the destinaton, append q and this vertex to visited list and return
+                    visited.append(q)
+                    visited.append(east_succssor_vertex)
+                    return visited
+                else:
+                    if(isInOpenList(east_succssor_vertex,open_List) == False):
+                        if(isVisited(east_succssor_vertex,visited) == False):
+                            open_List.append(east_succssor_vertex)
+        # west successor
+        west_succssor_row = q.row 
+        west_succssor_col = q.col - 1
+        
+        # check if in boundary(graph) and its not an obstacle
+        if(isInBoudary(west_succssor_row,west_succssor_col)):
+            if(graph[west_succssor_row][west_succssor_col] == 'air'):
+                h = heuristic((q.x-1),q.y,q.z,d_x,d_y,d_z)
+                west_succssor_vertex = Vertex((q.x-1),q.y,q.z,west_succssor_row,west_succssor_col,q,(q.g+1),h)
+                if(isDestination(west_succssor_vertex.x,west_succssor_vertex.y,west_succssor_vertex.z,d_x,d_y,d_z) == True):
+                    # We find the destinaton, append q and this vertex to visited list and return
+                    visited.append(q)
+                    visited.append(west_succssor_vertex)
+                    return visited
+                else:
+                    if(isInOpenList(west_succssor_vertex,open_List) == False):
+                        if(isVisited(west_succssor_vertex,visited) == False):
+                            open_List.append(west_succssor_vertex)
+        # push q to the visited list                
+        visited.append(q)
+    #end of while
+    return visited
+
+def traceThePath(visited,dest_x,dest_y,dest_z):
+    path = []
+    if(len(visited) == 0):
+        print("There is no path on the graph towards destination")
+        return path
+    last_vertex = visited[len(visited)-1]
+    if(isDestination(last_vertex.x,last_vertex.y,last_vertex.z,dest_x,dest_y,dest_z) == False):
+        print("The destination is on the graph, but there is no path towards the destination")
+        return path
+    else:
+        while(last_vertex != None):
+            path.append(last_vertex)
+            last_vertex = last_vertex.parent
+    path.reverse()
+    return path
+    
+def movement(agent_host,v):
+    agent_location = find_agent_location(agent_host)
+    dx = v.x - agent_location[0]
+    dy = v.y - agent_location[1]
+    dz = v.z - agent_location[2]
+    distance = math.sqrt(dx * dx + dy * dy + dz * dz)
+    yaw = -math.atan2(dx, dz) * 180 / math.pi
+    pitch = math.atan2(dy, distance)
+    pitch_degrees = math.degrees(pitch)
+
+    if(dx>0):
+        agent_host.sendCommand(f"setYaw {yaw}")
+        agent_host.sendCommand(f"setPitch {pitch_degrees}")
+        agent_host.sendCommand("moveeast 1")
+    elif(dx<0):
+        agent_host.sendCommand(f"setYaw {yaw}")
+        agent_host.sendCommand(f"setPitch {pitch_degrees}")
+        agent_host.sendCommand("movewest 1")
+    elif(dz>0):
+        agent_host.sendCommand(f"setYaw {yaw}")
+        agent_host.sendCommand(f"setPitch {pitch_degrees}")
+        agent_host.sendCommand("movesouth 1")
+    elif(dz<0):
+        agent_host.sendCommand(f"setYaw {yaw}")
+        agent_host.sendCommand(f"setPitch {pitch_degrees}")
+        agent_host.sendCommand("movenorth 1")
+    
+
+##########################################################
+def find_entity_location(agent_host,entityName):
+    world_state = agent_host.getWorldState()
+    while world_state.number_of_observations_since_last_state == 0:
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        if world_state.is_mission_running == False:
+            return None
+    msg = world_state.observations[-1].text
+    observations = json.loads(msg)
+    #print(observations)
+    if 'entities' in observations:
+        for entity in observations['entities']:
+            if entity['name'] == entityName:
+                return (entity['x'], entity['y'], entity['z'])
+    return None
+def find_agent_location(agent_host):
+    world_state = agent_host.getWorldState()
+    while world_state.number_of_observations_since_last_state == 0:
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        if world_state.is_mission_running == False:
+            return None
+    msg = world_state.observations[-1].text
+    observations = json.loads(msg)
+    if 'entities' in observations:
+        for entity in observations['entities']:
+            if entity['name'] == 'Agent':
+                return (entity['x'], entity['y'], entity['z'])
+    return None
+
+def move_to(agent_host,entityName):
+    entity_location = find_entity_location(agent_host,entityName)
+    agent_location = find_agent_location(agent_host)
+    if entity_location is None:
+        print("There is no " +entityName+ " nearby the agent")
+        return
+    if agent_location is None:
+        return
+
+    visited_nodes = Astar_search(agent_host,agent_location,entity_location)
+    path = traceThePath(visited_nodes,entity_location[0],entity_location[1],entity_location[2])
+    if(len(path) == 0):
+        print("No path Found")
+        return
+    
+    for v in path:
+        movement(agent_host,v)
+
+    return 
+
+# Brute force
+# def move_to(agent_host,entityName):       
+#     loopCount = 0
+#     while(1):
+#         # time.sleep(2)
+#         # msg = world_state.observations[-1].text
+#         # observations = json.loads(msg)
+#         # print(observations)
+#         entity_location = find_entity_location(agent_host,entityName)
+#         agent_location = find_agent_location(agent_host)
+#         if entity_location is None:
+#             print("There is no " +entityName+ " nearby the agent")
+#             return
+#         if agent_location is None:
+#             return
+
+#         target_x,target_y,target_z = entity_location[0],entity_location[1],entity_location[2]
+#         current_x, current_y, current_z = agent_location[0],agent_location[1],agent_location[2]
+
+#         # check to keep agent positioned correctly before a discrete move command
+#         if current_x % 1 != 0.5:
+#             agent_host.sendCommand(f"tpx {math.floor (current_x) + 0.5}")
+#         if current_z % 1 != 0.5:
+#             agent_host.sendCommand(f"tpz {math.floor (current_z) + 0.5}")
+
+#         dx = target_x - current_x
+#         dy = target_y - current_y
+#         dz = target_z - current_z
+#         distance = math.sqrt(dx * dx + dy * dy + dz * dz)
+#         yaw = -math.atan2(dx, dz) * 180 / math.pi
+#         pitch = math.atan2(dy, distance)
+#         pitch_degrees = math.degrees(pitch)
+#         # print(pitch)
+#         # print("agent location: (",current_x,current_y,current_z,")\n")
+#         # print("pigs location: (",target_x,target_y,target_z,")\n")
+#         # print("dx,dy,dz,distance are: (",dx,dy,dz,distance,")\n")
+
+#         if distance <= 1:
+#             agent_host.sendCommand("strafe 0")
+#             agent_host.sendCommand(f"setYaw {yaw}")
+#             agent_host.sendCommand(f"setPitch {pitch_degrees}")
+#             print("\nAlready at "+ entityName+"'s location.")
+#             return 
+
+#         else:
+#             if(loopCount == 0):
+#                 print("\nMoving towards "+ entityName+"'s location...",end="")
+#             else:
+#                 print(".",end="")
+            
+#             if(dx>0):
+#                 agent_host.sendCommand(f"setYaw {yaw}")
+#                 agent_host.sendCommand(f"setPitch {pitch_degrees}")
+#                 agent_host.sendCommand("moveeast 1")
+#             elif(dx<0):
+#                 agent_host.sendCommand(f"setYaw {yaw}")
+#                 agent_host.sendCommand(f"setPitch {pitch_degrees}")
+#                 agent_host.sendCommand("movewest 1")
+#             elif(dx == 0):
+#                 agent_host.sendCommand("strafe 0")
+
+#             if(dz>0):
+#                 agent_host.sendCommand(f"setYaw {yaw}")
+#                 agent_host.sendCommand(f"setPitch {pitch_degrees}")
+#                 agent_host.sendCommand("movesouth 1")
+#             elif(dz<0):
+#                 agent_host.sendCommand(f"setYaw {yaw}")
+#                 agent_host.sendCommand(f"setPitch {pitch_degrees}")
+#                 agent_host.sendCommand("movenorth 1")
+#             elif(dz==0):
+#                 agent_host.sendCommand("strafe 0")
+#         loopCount+=1
